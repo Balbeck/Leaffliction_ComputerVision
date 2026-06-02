2b90460e3562b76ee0f3920fbe589d4550fc6f18
Balbeck <albecker.hector@gmail.com>
2026-06-02 20:12:28 +0200

    🐍 feat: Python3.11 + Lib

--- init_env.sh
+++ init_env.sh
@@ -2,15 +2,17 @@
 # To Run: `source ./launch_env.sh`
 
 VENV_DIR=".LeafflictionVenv"
+PYTHON_BIN="$(brew --prefix python@3.11)/bin/python3.11"
 
 if [ ! -d "$VENV_DIR" ]; then
-	echo "🐍 Creation du venv ($VENV_DIR)..."
-	python3 -m venv "$VENV_DIR"
+	echo "🐍 Creation du venv [ ($VENV_DIR) ] avec Python 3.11..."
+	# python3 -m venv "$VENV_DIR"
+	"$PYTHON_BIN" -m venv "$VENV_DIR"
 fi
 
 echo "🏗️ Installation des dependances..."
 "$VENV_DIR/bin/pip" install --quiet --upgrade pip --no-cache-dir
-"$VENV_DIR/bin/pip" install --quiet -r requirements.txt --no-cache-dir
+"$VENV_DIR/bin/pip" install -r requirements.txt --no-cache-dir
 
 source "$VENV_DIR/bin/activate"
 echo "📦 Dependances installees, ✅ Init Done! ..."
